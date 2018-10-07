4de2f5d593e39d7dff4cbac3b314bc3dba332ebc
sergio <sergio.laiz87@gmail.com>
2018-10-07 21:28:00 +0200

    residencia actual

--- prueba.java
+++ prueba.java
@@ -3,6 +3,7 @@
     	public static void main(String[] args) {		
     		System.out.println("Sergio Laiz Lozano");
     		System.out.println("Leon");
+    		System.out.println("San Andres del Rabendo");
     	}
        
 }
